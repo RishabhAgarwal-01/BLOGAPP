d74a70de85a5be98a670ac673e0b16fcc1ef2e24
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-06-05 11:02:22 +0530

    created signup api routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes  from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //middleware to parse the json data
 
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -12,9 +17,10 @@ mongoose.connect(process.env.MONGO)
     console.log(error);
 })
 
-const app = express();
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
 
 app.listen(8000, ()=>{
     console.log("Server is running on port 8000");
