c1d8178007085dbf4cb84995e3ccc8a7a42e011b
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-06-09 22:43:23 +0530

    completed the signin ui functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Label, TextInput, Alert, Spinner } from "flowbite-react";
 
-function Signin() {
+function SignIn() {
+  const [formData, setFormData] = useState({}); //state storing the formData as an object
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() }); //we will update the formData by making it a keyvalue pair whichever input is calling the handleChange will give it's id and value
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("please fill out all fields");
+    }
+    try {
+      //cleaning previous stored errors if any and setting the Loading state true every time the submit happen and stop rendering the submit button till res is get.
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json(); //awaiting res from the server side it will return either "Signup sucessful" or an object {sucess:false, statusCode: statusCode, message:message} from server side if any error occur
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false); //if everything it right then just change to false and it will stop rendering the loading effect after submit happened and res is ok
+
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      //handling client side error while doing signup submit
+      setErrorMessage(error.message);
+      setLoading(false); //if res not okay or some client side error like no internet happen then
+      //after error message is displyed again make the loading false to stop rendering the loading effect and render the submit button again
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/*left side div showing the logo*/}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-green-500 to-blue-500 text-white rounded-lg">
+              Rishabh's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a personal project. You can sign in with your email and
+            password Or with Google.
+          </p>
+        </div>
+
+        {/*div for the right side showing the signup form*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="your password" />
+              <TextInput
+                type="password"
+                placeholder="********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="greenToBlue"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? <Spinner size="sm" /> : "SignIn"}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign-Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Signin
+export default SignIn;
