d091addd9a3e898996740793f1a12bd6b920b02c
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-06-09 22:28:46 +0530

    create signin route in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
 import { errorHandler } from '../utils/error.js';
-
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next)=>{
     const {username, email, password} =req.body;
@@ -25,5 +25,36 @@ export const signup = async(req, res, next)=>{
      } catch (error) {
        next(error); //error from system, not the custom one also getting passed to the middleware in index.js
      }
-        
+}
+
+export const signin = async (req, res, next)=>{
+  const {email, password} = req.body;
+  
+  if(!email || !password || email==="" || password ===''){
+       next(errorHandler(400, "All fields are requires"));
+  }
+
+  try {
+     const validUser = await User.findOne({email});
+     if(!validUser){
+      return next(errorHandler(404, "User not found"));
+     }
+     
+     const validPassword = bcryptjs.compareSync(password, validUser.password);
+     if(!validPassword){
+      return next(errorHandler(400, "Invalid Password"));
+     }
+     
+     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      
+     //this is done so that when sending response back to the user, we don't send the password along with it.
+     //password: pass will remove the password from the object, ...rest will copy the rest of the data from the validUser._doc which is the document instance in the mongodb
+     const {password: pass, ...rest}= validUser._doc;
+    
+     res.status(200).cookie('access_token', token,{ 
+        httpOnly: true}).json(rest);
+
+    } catch (error) {
+      next(error)
+  }
 }
