758a0e286d9796d6e9166df46d9a8b239856039a
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-06-27 23:33:35 +0530

    added api route for get posts along with filtering and searching logic

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -25,3 +25,52 @@ export const create = async(req, res, next) =>{
      next(error);
   }
 }
+
+export const getPosts = async (req, res, next)=>{
+   try {
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      const limit = parseInt(req.query.limit) || 9;
+      const sortDirection =req.query.order === 'asc' ? 1: -1;
+
+      //dynamic query for the search filter operation for posts
+      const posts = await Post.find({
+        ...(req.query.userId && { userId: req.query.userId }),
+        ...(req.query.category && { category: req.query.category }),
+        ...(req.query.slug && { slug: req.query.slug }),
+        ...(req.query.postId && { _id: req.query.postId }),
+        ...(req.query.searchTerm && {
+          $or: [
+            { title: { $regex: req.query.searchTerm, $options: 'i' } },
+            { content: { $regex: req.query.searchTerm, $options: 'i' } },
+          ],
+        }),
+      }) 
+      .sort({updatedAt : sortDirection})
+      .skip(startIndex)
+      .limit(limit);
+
+      //total post
+      const totalPosts = await Post.countDocuments();
+
+      //total posts in last month logic
+      const now = new Date(); //today
+      const oneMonthAgo = new Date(  //month before
+        now.getFullYear(),
+        now.getMonth()-1,
+        now.getDate()
+      );
+
+      const lastMonthPosts = await Post.countDocuments({
+        createdAt: {$gte: oneMonthAgo}, //last month posts but created at greater than oneMonthAgo
+      })
+       
+      res.status(200).json({
+        posts,
+        totalPosts,
+        lastMonthPosts,
+      });
+
+   } catch (error) {
+     next(error);
+   }
+}
