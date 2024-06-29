89978bf3dd2e000be246b37e4becce64f37e0d92
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-06-29 12:55:09 +0530

    added delete comment api  route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {verifyToken} from "../utils/verifyUser.js"
-import {createComment, likeComment} from "../controllers/comment.controller.js"
+import {createComment, likeComment, deleteComment} from "../controllers/comment.controller.js"
 import { getPostComments } from '../controllers/comment.controller.js';
+
+
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment )
-
+router.put('/deleteComment/:commentId', verifyToken, deleteComment )
 export default router;
