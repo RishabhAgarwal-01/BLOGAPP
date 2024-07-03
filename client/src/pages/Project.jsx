1475a1590cb273dee4b2106933798473a5508e02
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-03 10:08:05 +0530

    made changes to project page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -5,7 +5,7 @@ function Project() {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Pojects</h1>
-      <p className='text-md text-gray-500'>To add more projects soon !!</p>
+      <p className='text-md text-gray-500'>dding more projects soon !!</p>
       <CallToAction />
     </div>
   )
